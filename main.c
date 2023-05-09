e9aba6d7c6af4cd10446a3d5977304783ff8d34f
abelov <d_mak@rambler.ru>
2023-05-09 18:48:28 +0300

    add prototype arg to main

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <my_func.h>
 
-int main() {
+int main(int argc, char **argv)
+{
+    int option = 0;
+
+    if(argc == 1)
+    {
+        printf("-h for help.\n");
+        return 0;
+    }
+    do
+    {
+        option = getopt(argc,argv,"hpgf:m:y:");
+        if(option == -1)
+            break;
+        
+        switch (option)
+        {            
+            case 'h':
+                printf("Equation root finder application.\n");
+                printf("-o show of intersection points of functions.\n");
+                printf("-p show of interation numbers.\n");
+                printf("-t test function xxXX (xx - function, XX - test).\n");
+                return 0;
+                break;
+            case 'o':
+                
+                break;
+            case 'p':
+                
+                break;
+            case 't':
+                
+                break;
+            case '?':
+                printf("Unknown option.\n");
+                printf("-h for help.\n");
+                break;
+        }
+    } while(option != -1);
+
     float xl = 1, xr = 4, eps = 1e-3;
     
     printf("Linear search root: %f\n", rootFindLineSearch(f1_2, xl, xr, eps));
